a6ec0f5de1370b1c081868afe66c8af30bbb0214
hemanth <hemanth.sp03@gmail.com>
2023-02-11 16:00:27 +0530

    Added telescope's filebrowser extension

--- after/plugin/telescope_config.lua
+++ after/plugin/telescope_config.lua
@@ -12,6 +12,8 @@ require('telescope').setup {
 }
 
 
+require('telescope').load_extension "file_browser"
+
 -- Enable telescope fzf native, if installed
 pcall(require('telescope').load_extension, 'fzf')
 
@@ -31,3 +33,4 @@ vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+vim.keymap.set('n', '<leader>t', ':Telescope file_browser<CR>', { desc = 'File Browser' })
