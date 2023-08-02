002f333ebfa11e9aa060e0223b3cf74e9c5604e3
Prashantha S P <prashanth.sp98@gmail.com>
2023-08-02 21:42:52 +0530

    Added key binding <leader>r to comile and run few language

---
+++ lua/custom_key_maps.lua
@@ -0,0 +1,25 @@
+local M = {}
+
+M.SetMake = function ()
+    print()
+    local fileType = vim.api.nvim_buf_get_option(0, 'filetype')
+
+    local filetype_comp_map_table = {}
+
+    filetype_comp_map_table.python = "python3 %"
+    filetype_comp_map_table.cpp = "g++ % -o exec.out && ./exec.out"
+    filetype_comp_map_table.c = "gcc % -o exec.out && ./exec.out"
+    filetype_comp_map_table.rust = "cargo build"
+    filetype_comp_map_table.lua = "cargo build"
+
+    print(fileType)
+
+    local buf = vim.api.nvim_get_current_buf()
+
+    vim.api.nvim_buf_set_option(buf, "makeprg", filetype_comp_map_table[fileType])
+
+    vim.keymap.set("n", "<leader>r", ":make <CR>")
+
+end
+
+return M
