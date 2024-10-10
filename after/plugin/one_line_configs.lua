49a7d2a0bc70fea980129abe3ce206dcb0841901
Hemanthsp999 <hemanth.sp03@gmail.com>
2024-10-10 20:08:29 +0530

    Fixed booting error from indent_block_line in latest version of nvim

--- after/plugin/one_line_configs.lua
+++ after/plugin/one_line_configs.lua
@@ -7,11 +7,17 @@ require("neodev").setup()
 -- Enable Comment.nvim
 require("Comment").setup()
 
+
 -- Enable `lukas-reineke/indent-blankline.nvim`
 -- See `:help indent_blankline.txt`
-require("indent_blankline").setup({
-    char = "┊",
-    show_trailing_blankline_indent = false,
+require("ibl").setup({
+    indent = {
+        char = "┊",
+    },
+    scope = {
+        show_start = true,
+        show_end = false,
+    },
 })
 
 vim.keymap.set('n', '<leader>ut', vim.cmd.UndotreeToggle)
