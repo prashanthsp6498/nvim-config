1c37070ff6906609fd7435864d201db6df7aa146
Prashantha S P <prashanth.sp98@gmail.com>
2024-12-25 17:30:15 +0530

    Refactor : few plugin setup initialized in lazy way

--- after/plugin/one_line_configs.lua
+++ after/plugin/one_line_configs.lua
@@ -1,26 +1,3 @@
--- Turn on lsp status information
-require("fidget").setup()
-
--- Setup neovim lua configuration
-require("neodev").setup()
-
--- Enable Comment.nvim
-require("Comment").setup()
-
-
--- Enable `lukas-reineke/indent-blankline.nvim`
--- See `:help indent_blankline.txt`
-require("ibl").setup({
-    indent = {
-        char = "┊",
-    },
-    scope = {
-        show_start = true,
-        show_end = false,
-    },
-})
-
-vim.keymap.set('n', '<leader>ut', vim.cmd.UndotreeToggle)
 vim.opt.shortmess:append("I")
 
 vim.cmd [[ highlight Normal  guibg=#121212 gui=NONE ctermfg=254 ctermbg=235 cterm=NONE
