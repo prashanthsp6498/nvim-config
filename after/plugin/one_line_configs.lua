39dc7c0f94870ff586c2ae7c0ab0e854ff34dbc3
Prashantha S P <prashanth.sp98@gmail.com>
2024-12-25 17:07:55 +0530

    Change bg color

--- after/plugin/one_line_configs.lua
+++ after/plugin/one_line_configs.lua
@@ -22,3 +22,6 @@ require("ibl").setup({
 
 vim.keymap.set('n', '<leader>ut', vim.cmd.UndotreeToggle)
 vim.opt.shortmess:append("I")
+
+vim.cmd [[ highlight Normal  guibg=#121212 gui=NONE ctermfg=254 ctermbg=235 cterm=NONE
+           highlight NonText  guibg=#121212 gui=NONE ctermfg=246 ctermbg=235 cterm=NONE ]]
